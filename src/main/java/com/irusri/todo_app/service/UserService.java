abad7cbdfdc77bf2ddf5bb1de1f82edadc9ff51e
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2024-11-03 19:47:22 +0530

    Create Service Layer for User

---
+++ src/main/java/com/irusri/todo_app/service/UserService.java
@@ -0,0 +1,23 @@
+package com.irusri.todo_app.service;
+
+import com.irusri.todo_app.model.User;
+import com.irusri.todo_app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
+}
