d3ced868e3826dfced09a8b392031c7194ccf26e
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2024-11-03 19:41:27 +0530

    User Model created

---
+++ src/main/java/com/irusri/todo_app/model/User.java
@@ -0,0 +1,20 @@
+package com.irusri.todo_app.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
